1299aad2d8d89824f6f1ee7d4c9865d5158a3c1f
unicorngirl2010 <86110608+unicorngirl2010@users.noreply.github.com>
2022-05-01 16:35:21 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -13,7 +13,7 @@ firebase.initializeApp(firebaseConfig);
 //ADD YOUR FIREBASE LINKS
 function addUser() {
     user_name = document.getElementById("user_name").value;
-    firebase.datebase().ref("/").child(user_name).update({
+    firebase.database().ref("/").child(user_name).update({
         purpose: "adding user"
     });
 }
